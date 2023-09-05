d31209e12c7c0ca30497e685704180a15b39a2e8
84h3p <43922329+84h3p@users.noreply.github.com>
2023-09-05 14:32:08 +0000

    Update client.lua

--- client.lua
+++ client.lua
@@ -39,20 +39,3 @@ RegisterCommand('taxi', function(source, args)
     RemoveBlip(destinationBlip)
     local  customer = SetEntityAsNoLongerNeeded()
 end, false)
-
-
--- Get coord command
-RegisterCommand('getcoord', function(source, args)
-    TriggerEvent('chat:addMessage', {
-        args = {'x: ', Floor((GetEntityCoords(GetPlayerPed(-1), true).x)*100)/100,}
-    })
-    TriggerEvent('chat:addMessage', {
-        args = {'y: ', Floor((GetEntityCoords(GetPlayerPed(-1), true).y)*100)/100,}
-    })
-    TriggerEvent('chat:addMessage', {
-        args = {'z: ', Floor((GetEntityCoords(GetPlayerPed(-1), true).z)*100)/100,}
-    })
-    TriggerEvent('chat:addMessage', {
-        args = {'h: ', Floor(GetEntityHeading(PlayerPedId()))}
-    })
-end, false)
